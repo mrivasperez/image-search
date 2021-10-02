0c1f266c1333f697cb77b48ebcdb3b33e8adba68
Miguel Rivas Perez <miguel@Miguels-MacBook-Pro.local>
2021-10-02 10:30:28 -0700

    update ImageCard.js

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 
 export class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+    // create reference to image to get the properties of the image
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // get the height of image when the page is updated after getting image
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    // get heigh of each ImageList
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
     const { alt_description, urls } = this.props.image;
+
     return (
       <div>
-        <img alt={alt_description} src={urls.regular} />
+        <img ref={this.imageRef} alt={alt_description} src={urls.regular} />
       </div>
     );
   }
